5bc92dd8be5858edae4cef607747f0c2d53a364a
T-kinghou <121077472+T-kinghou@users.noreply.github.com>
2023-09-30 21:15:51 +0800

    进行了修改

--- main/java/MyTest_git.java
+++ main/java/MyTest_git.java
@@ -3,5 +3,10 @@
  * version 1.0
  */
 public class MyTest_git {
+    private String name;
+    private int age;
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
 
 }
